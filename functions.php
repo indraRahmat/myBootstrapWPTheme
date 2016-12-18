56a19688a295c3b2084b08467a6965b64b145abe
indra <indra.rahmat88@gmail.com>
2016-12-18 22:57:33 +1100

    adding dynamic posting plus image

--- functions.php
+++ functions.php
@@ -4,6 +4,9 @@ require_once('wp_bootstrap_navwalker.php');
 
 //theme support
 function wpb_theme_setup(){
+  //
+  add_theme_support('post-thumbnails');
+
   //nav menu
   register_nav_menus(array(
     'primary' => __('Primary Menu')
@@ -11,3 +14,10 @@ function wpb_theme_setup(){
 }
 
 add_action('after_setup_theme','wpb_theme_setup');
+
+//excerpt length control
+function set_excerpt_length(){
+  return 50;
+}
+
+add_filter('excerpt_length', set_excerpt_length);
